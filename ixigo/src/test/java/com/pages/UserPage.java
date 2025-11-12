93351540d923949a430620fa543dd63301c8aedd
sidda-chitti713 <SIDDA-CHITTI.SIDDA-CHITTI@CAPGEMINI.COM>
2025-11-12 19:46:43 +0530

    Hotels-fourth-commit

--- ixigo/src/test/java/com/pages/UserPage.java
+++ ixigo/src/test/java/com/pages/UserPage.java
@@ -4,15 +4,16 @@ package com.pages;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.setup.BaseSteps;
+
 
 public class UserPage extends BaseSteps {
 	private WebDriver driver;
@@ -27,18 +28,31 @@ public class UserPage extends BaseSteps {
 
 	@FindBy(css = "button[data-testid=\"search-hotels\"] > div.flex.items-center.gap-5.font-medium")
 	private WebElement searchButton;
-	
+
 	public void clickSearch() {
 		searchButton.click();
 	}
 
 
 
+	public void clickView() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-	//private By availabilityCheckInDate = By.xpath("//div[@data-placeholder='Check-in']");
-	//private By availabilityCheckOutDate = By.xpath("//div[@data-placeholder='Check-out']");
+	    // Wait for the first hotel card link to be clickable
+	    WebElement viewButton = wait.until(ExpectedConditions.elementToBeClickable(
+	        By.xpath("(//div[contains(@class,'shrink-0') and contains(@class,'rounded-xl') and contains(@class,'h-[258px]')]//a)[1]")
+	    ));
+
+	    // Click the hotel card link (this opens a new tab)
+	    viewButton.click();
+
+	    // Switch to the newly opened tab
+	    BrowserUtils.switchToNewTab(driver);
+	}
+	
 
 
+/*	
 
 
 	public void enterDestination(String destination) {
@@ -54,41 +68,41 @@ public class UserPage extends BaseSteps {
 	}
 
 
+	@FindBy(css = "input[data-testid='checkin-input']")
+	private WebElement checkInInput;
 
-	    @FindBy(css = "input[data-testid='checkin-input']")
-	    private WebElement checkInInput;
+	@FindBy(css = "input[data-testid='checkout-input']")
+	private WebElement checkOutInput;
 
-	    @FindBy(css = "input[data-testid='checkout-input']")
-	    private WebElement checkOutInput;
+	@FindBy(css = "input[placeholder=\"Rooms & Guests\"]")
+	private WebElement roomGuestInput;
 
-	//    @FindBy(css = "input[placeholder=\"Rooms & Guests\"]")
-	//    private WebElement roomGuestInput;
 
-	//    public void clickHotelsTab() {
-	//        hotelsTab.click();
-	//    }
+	public void enterCheckInDate(String date) {
+		checkInInput.sendKeys(date);
+	}
 
+	public void enterCheckOutDate(String date) {
+		checkOutInput.sendKeys(date);
+	}
+
+
+
+	public void enterRoomGuestDetails(String rooms, String guests) {
+		roomGuestInput.click();
+
+	}
+
+	@FindBy(css = "button.SearchForm_animated_btn__WTjFh > div.flex.items-center.gap-5.font-medium")
+	private WebElement searchButton1;
 	
 
-//	    public void enterDestination(String destination) {
-//	        destinationInput.sendKeys(destination);
-//	    }
+
+*/
 
 
-	    public void enterCheckInDate(String date) {
-	        checkInInput.sendKeys(date);
-	    }
 
-	    public void enterCheckOutDate(String date) {
-	        checkOutInput.sendKeys(date);
-	    }
 
-	//    public void enterRoomGuestDetails(String rooms, String guests) {
-	//        roomGuestInput.click();
-	//        // Add logic to select rooms and guests from dropdown
-	//    }
-
-	//    public void clickSearch1() {
-	//        searchButton.click();
-	//    }
 }
+
+
